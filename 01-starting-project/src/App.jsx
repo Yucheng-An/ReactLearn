e0587c8e53c25b1299fc52105889b96640b9b217
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 14:47:29 -0700

    Auto-commit JSX on save for COMP 227

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,7 +5,7 @@ function Header(){
                  alt = "Stylized atom" />
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are going to build!
+                This is 
             </p>
         </header>
     )
