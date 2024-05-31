fbf7f473556891a6538c9e48fa72637c52d1c61a
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:49:07 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Display.jsx
+++ PersonalProject/todolist/src/Components/Display.jsx
@@ -2,7 +2,15 @@ import react from'react';
 import '../App.css';
 
 function Display(){
-
+return (
+    <div id="core-concepts">
+        <ul>
+            {currentList.map((item) => (
+                <li key={item.id}>{item.title}</li>
+            ))}
+        </ul>
+    </div>
+)
 }
 
 
