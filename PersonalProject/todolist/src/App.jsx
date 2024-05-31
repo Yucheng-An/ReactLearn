c75929c055cab034b9ba5b00549bec5ce52b1ba1
Yucheng-An <yuc.an2019@gmail.com>
2024-05-31 01:20:26 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState , useEffect} from 'react'
 import axios from "axios";
-import './App.css'
 import Header from './Components/Header'
 import Display from './Components/Display'
 import Submit from './Components/Submit'
