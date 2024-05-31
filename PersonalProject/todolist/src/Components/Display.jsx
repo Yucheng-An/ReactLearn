10615703d9599970314076c6346d7a93e3a5c9c5
Yucheng-An <yuc.an2019@gmail.com>
2024-05-31 00:55:37 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Display.jsx
+++ PersonalProject/todolist/src/Components/Display.jsx
@@ -6,9 +6,7 @@ function Display(props){
     <div id = "center-container">
         <div id = "ulBox">
             {props.currentList.map((item) => (
-                <input type = "checkbox" key={item.id}>
                 <p id = "task" key={item.id}>{item.title}</p>
-
             ))}
         </div>
     </div>
