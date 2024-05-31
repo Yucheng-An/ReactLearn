a1d48ed1a66d61550ae76c8f26522ca0defe6fd3
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:57:56 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const task = event.target.value
+    const task = event.target.value.text
     const id = currentList.length + 1
     const newTask = {
       id: id,
