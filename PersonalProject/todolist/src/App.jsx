d2d5a75fc0ead8d171fe7edb1b4543fb959b9b58
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 22:04:19 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const [newTask, setNewTask] = useState(null)
 
 
-  const response = await fetch('http://localhost:3001/todolist')
+  const response.then()fetch('http://localhost:3001/todolist')
   const data = await response.json()
   console.log(data)
   setCurrentList(data)
