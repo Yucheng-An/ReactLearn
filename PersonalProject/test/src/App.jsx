6a026fa0f0201f16520125a84ece9e3be3fdb402
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 22:38:42 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/test/src/App.jsx
+++ PersonalProject/test/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
    const handleTaskSubmit = (event) =>{
         event.preventDefault()
        console.log("Task Submitted")
-       console.log(event.target[1].value)
+       console.log(event.target[2].value)
    }
   return (
     <div>
