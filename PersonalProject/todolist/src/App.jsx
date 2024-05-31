fe6e62d81d1bca06889f4330ddef1dad466201df
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 22:05:35 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -5,21 +5,12 @@ import Display from './Components/Display'
 import Submit from './Components/Submit'
 import data from './assets/data'
 
-fetch('http://localhost:3001/todolist').then(r => r.json()).then(data => {
-  setCurrentList(data)
-})
-
 
 
 function App() {
   const [currentList, setCurrentList] = useState(null)
   const [newTask, setNewTask] = useState(null)
-
-
-
-  fetch('http://localhost:3001/todolist').then(r => r.json()).then(data => {
-    setCurrentList(data)
-  })
+  
 
   return (
       <div>
