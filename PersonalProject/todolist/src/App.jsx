4e09316a239a78cd7442a54e3c352e4e7a82e2bf
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:41:05 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header'
 
+const list = []
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
         <Header />
-        
+
     </div>
   )
 }
