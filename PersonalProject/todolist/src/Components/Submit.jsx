fb92f7c89e973b63b235a0ea682f4f9320c48a09
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 22:18:28 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Submit.jsx
+++ PersonalProject/todolist/src/Components/Submit.jsx
@@ -13,7 +13,7 @@ function Submit(props){
             title: task,
             completed: false
         }
-        axios.post("http://localhost:")
+        axios.post("http://localhost:3001/todolist")
         props.currentList.push(newTask)
         console.log(props.currentList)
         props.setCurrentList(props.currentList)
