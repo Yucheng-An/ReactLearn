4404f0dabee4bb244ce8f0f733a58d7aa4d0271b
Yucheng-An <yuc.an2019@gmail.com>
2024-05-31 00:52:29 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Display.jsx
+++ PersonalProject/todolist/src/Components/Display.jsx
@@ -6,7 +6,7 @@ function Display(props){
     <div id = "center-container">
         <ul id = "ulBox">
             {props.currentList.map((item) => (
-                <input id = "task" key={item.id}>{item.title}</li>
+                <input id = "task" key={item.id}>{item.title}</input>
             ))}
         </ul>
     </div>
