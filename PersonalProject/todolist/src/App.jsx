b8f7557c95f2dd50f61d61889dd426f660b07e0c
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 21:58:59 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import './App.css'
 import Header from './Components/Header'
 import Display from './Components/Display'
@@ -14,7 +14,7 @@ function App() {
   const [newTask, setNewTask] = useState(null)
 
   useEffect(()=>{
-
+    
   })
   return (
     <div>
