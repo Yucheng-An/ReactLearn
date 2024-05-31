b01b4cc55614495ddc94d894eb4840b1ff8e1b6a
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:41:33 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -3,6 +3,22 @@ import './App.css'
 import Header from './Components/Header'
 
 const list = [
+  {
+    id: 1,
+    title: 'Go to the gym',
+    completed: false
+  },
+  {
+    id: 2,
+    title: 'Do laundry',
+    completed: true
+  },
+  {
+    id: 3,
+    title: 'Finish project',
+    completed: false
+  }
+]
 function App() {
   const [count, setCount] = useState(0)
 
