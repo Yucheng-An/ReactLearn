0aee160208f5d76b487eca018dee9b5a1c616a83
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:51:44 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Submit.jsx
+++ PersonalProject/todolist/src/Components/Submit.jsx
@@ -5,7 +5,8 @@ import '../App.css';
 function Submit(props){
     return (
         <form>
-    
+            <input type="text" placeholder="Add a new task" />
+            <button type="submit">Add</button>
         </form>
     )
 }
