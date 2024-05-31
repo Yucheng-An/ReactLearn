5cffb83c699cfa5b51d026cf68ec4f6c9b239af6
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:59:52 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/Components/Submit.jsx
+++ PersonalProject/todolist/src/Components/Submit.jsx
@@ -5,6 +5,15 @@ import '../App.css';
 function Submit(props){
     const onChanege = (event) => {
         event.preventDefault()
+        const task = event.target.value.text
+        const id = props.currentList.length + 1
+        const newTask = {
+            id: id,
+            title: task,
+            completed: false
+        }
+        props.list.push(newTask)
+        props.setCurrentList(props.list)
     
     }
     return (
