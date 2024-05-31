a74520bddae338122c9a47e16467525849480e86
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 19:57:02 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -42,7 +42,7 @@ function App() {
     <div>
         <Header />
         <Display currentList={currentList} />
-        <Submit handleSubmit={}/>
+        <Submit handleSubmit={handleSubmit}/>
     </div>
   )
 }
