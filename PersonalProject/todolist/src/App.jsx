c4a5d9c309f8b7004d49b4467fdc5fc3ab81b219
Yucheng-An <yuc.an2019@gmail.com>
2024-05-30 22:03:09 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -10,7 +10,7 @@ import data from './assets/data'
 
 
 function App() {
-    const [currentList, setCurrentList] = useState()
+  const [currentList, setCurrentList] = useState()
   const [newTask, setNewTask] = useState(null)
 
 
