f7b2adc85b9bcc86fdad079e0554edcaeb9ab87d
Yucheng-An <yuc.an2019@gmail.com>
2024-05-31 01:47:18 -0700

    Auto-commit JSX on save for COMP 227

--- PersonalProject/todolist/src/App.jsx
+++ PersonalProject/todolist/src/App.jsx
@@ -1,35 +1,33 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
+import { useState , useEffect} from 'react'
+import axios from "axios";
+import Header from './Components/Header'
+import Display from './Components/Display'
+import Submit from './Components/Submit'
 
-export default function PinnedSubheaderList() {
-    return (
-        <List
-            sx={{
-                width: '100%',
-                maxWidth: 360,
-                bgcolor: 'background.paper',
-                position: 'relative',
-                overflow: 'auto',
-                maxHeight: 300,
-                '& ul': { padding: 0 },
-            }}
-            subheader={<li />}
-        >
-            {[0, 1, 2, 3, 4].map((sectionId) => (
-                <li key={`section-${sectionId}`}>
-                    <ul>
-                        <ListSubheader>{`I'm sticky ${sectionId}`}</ListSubheader>
-                        {[0, 1, 2].map((item) => (
-                            <ListItem key={`item-${sectionId}-${item}`}>
-                                <ListItemText primary={`Item ${item}`} />
-                            </ListItem>
-                        ))}
-                    </ul>
-                </li>
-            ))}
-        </List>
-    );
+
+
+function App() {
+  const [currentList, setCurrentList] = useState([])
+  const [newTask, setNewTask] = useState(null)
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/todolist").then((response)=>{
+        setCurrentList(response.data)
+    }).catch(((error)=>console.log(error)))
+  }, [])
+
+
+  return (
+      <div>
+        <Header/>
+        <Display currentList={currentList}/>
+        <Submit currentList={currentList}
+                setCurrentList={setCurrentList}
+                newTask={newTask}
+                setNewTask={setNewTask}/>
+      </div>
+  )
 }
+
+export default App
