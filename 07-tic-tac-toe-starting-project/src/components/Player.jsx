a65870a629fa4b78b855ecb83a0ae4109aa7bdd3
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:01:33 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,7 +2,7 @@ import react, {useState} from 'react';
 
 
 
-export default function Player({initialName,symbol}) {
+export default function Player({initialName,symbol, is}) {
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
