53c20b3f5a918d2197686a405310d1d5c4f57445
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:32:34 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -5,7 +5,7 @@ export default function Log({turns}){
 
     return (
         <ol id = "log">
-            {turns.map((turn,index))}
+            {turns.map((turn,index) => <li></li>)}
         </ol>
     )
 }
