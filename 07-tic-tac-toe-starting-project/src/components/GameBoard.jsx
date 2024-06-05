bc95b8f146fd73b08490d8ea19d1815f1f2f3ea9
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:57:10 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -13,7 +13,7 @@ export default function GameBoard(){
 
     function handleSelectSqueare(rowIndex,colIndex){
         setGameBoard((prevGameBoard) => {
-            const updatedGameBoard = prevGameBoard.map((row,rowIndex) => {
+            const updatedGameBoard = prevGameBoard.map((row,rowI
             prevGameBoard[rowIndex][colIndex] = "X";
         })
     }
