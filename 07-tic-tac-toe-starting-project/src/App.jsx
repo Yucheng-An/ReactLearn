0089ac38198d4c2091cf73790f03d9eb5c4d85c9
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:01:54 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -10,13 +10,7 @@ function App() {
         <div id = "game-container">
             <ol id="players">
                <Player name="Player 1" symbol="X"/>
-                <li>
-                    <span className="player">
-                    <span className="player-name">Player 2</span>
-                    <span className="player-symbol">O</span>
-                    </span>
-                    <button>Edit</button>
-                </li>
+                <Player name="Player 2" symbol="O"/>
             </ol>
             Game Board
         </div>
