93579ac7563813c156ef461436e26ae9aa4f751c
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:17:49 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -13,8 +13,7 @@ export default function Player({name,symbol}) {
     let btnCaption = "Edit"
     
     if (isEditing){
-        playerName = <input type = "text" required />
-        btnCaption = "Save"
+        playerName = <input type = "text" required value ={}
     }
 
 
