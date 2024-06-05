40ecf13c6f5b039be4fa1fdbbfd0b9991851f68f
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:27:34 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -3,6 +3,7 @@ import react, {useState} from 'react';
 
 
 export default function Player({name,symbol}) {
+    
     const [isEditing, setIsEditing] = useState(false);
     function handleEditClick(){
         setIsEditing((prevState) => !prevState)
