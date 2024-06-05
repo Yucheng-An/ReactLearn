00afe2558da304770428f2fe6908d6fa878f90fd
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:03:21 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <div id = "game-container">
             <ol id="players" className = "highlight-player">
                <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-                <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'X'}/>
+                <Player initialName="Player 2" symbol="O" isActive={activePlayer === ''}/>
             </ol>
             <GameBoard onSelectSquare={handleSelectSquare}/>
         </div>
