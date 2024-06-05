ee9deabc6e76384646a30d368c79258d84f102b5
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:32:06 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -10,14 +10,14 @@ export default function Player({initialName,symbol}) {
     }
 
     function handleChange(event){
-        
+
     }
 
     let editablePlayerName = <span className = "player-name" >{playerName}</span>
     let btnCaption = "Edit"
     
     if (isEditing){
-        editablePlayerName = <input type = "text" required value ={playerName}/>
+        editablePlayerName = <input type = "text" required value ={playerName} onChange = {handleChange}/>
         btnCaption = "Save"
     }
 
