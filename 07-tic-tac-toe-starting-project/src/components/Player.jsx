3aa456b7c64e2daac60900394216c6711a26cc60
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:00:37 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,7 +1,14 @@
 import react from 'react';
 
 export default function Player() {
-    return(
+    return (
+        <li>
+                    <span className="player">
+                    <span className="player-name">Player 1</span>
+                    <span className="player-symbol">X</span>
+                    </span>
+            <button>Edit</button>
+        </li>
 
     )
 }
