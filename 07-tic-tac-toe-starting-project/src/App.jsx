bb159dfd1c590bae2fa17edc6761591ee56c6592
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:18:41 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         setActivePlayer((currentActivePlayer) => (currentActivePlayer === 'X' ? 'O' : 'X'));
         setGameTurns((prevTurns) => {
             let currentPlayer = 'X';
-            if (prevTurns.length > 0 && prevTurns[0])
+            if (prevTurns.length > 0 && prevTurns[0].player === 'X')
         });
     }
 
