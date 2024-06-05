707805822bfcfa8310b97c6f669f941484defe81
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 22:37:28 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
                     <span className="player-name">Player 1</span>
                     <span className="player-symbol">X</span>
                     </span>
+                    <button>E</button>
                 </li>
                 <li>
                     <span className="player-name">Player 2</span>
