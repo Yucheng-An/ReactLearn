7c08f4101cf6faa8ee6d364ceac5447aa9ba96d1
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:43:51 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -8,7 +8,7 @@ const initialGameBoard = [
 
 export default function GameBoard(){
     return <ol id="game-board">
-{initialGameBoard.map((row, rowIndex) => {
+        {initialGameBoard.map((row, rowIndex) => {
             return row.map((cell, cellIndex) => {
                 return <li key = {`${rowIndex}-${cellIndex}`} className="cell"></li>
             })
