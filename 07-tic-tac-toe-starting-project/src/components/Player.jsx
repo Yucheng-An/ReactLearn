05816430d5b57a4ae54c5030574653539d7d4c84
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:10:06 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -8,7 +8,7 @@ export default function Player({name,symbol}) {
         setIsEditing(true)
     }
 
-    let playerName = <span className = "" ></span>
+    let playerName = <span className = "player-name" ></span>
 
 
     return (
