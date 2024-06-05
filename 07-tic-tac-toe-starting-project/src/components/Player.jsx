81ee8c21755dee771cfda272506da82c402a307e
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:00:59 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player({name,symbol}) {
     return (
         <li>
                     <span className="player">
-                    <span className="player-name">Player 1</span>
+                    <span className="player-name">{name}</span>
                     <span className="player-symbol">X</span>
                     </span>
             <button>Edit</button>
