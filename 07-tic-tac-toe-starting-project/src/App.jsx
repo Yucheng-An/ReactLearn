dc7b4b1a90e606d67783015619bd7392b4784b9a
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:01:01 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
       <main>
         <div id = "game-container">
-            <ol id="players">
+            <ol id="players" className = "">
                <Player initialName="Player 1" symbol="X"/>
                 <Player initialName="Player 2" symbol="O"/>
             </ol>
