1f1b781812ea27b549e6029562b472e557d65256
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 23:02:30 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,8 +2,7 @@ import react from 'react';
 
 export default function Player({name,symbol}) {
     return (
-        <li>
-                    <span className="player">
+        <li><span className="player">
                     <span className="player-name">{name}</span>
                     <span className="player-symbol">{symbol}</span>
                     </span>
