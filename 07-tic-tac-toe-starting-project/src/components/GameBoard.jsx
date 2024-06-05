5a3836adddac949b1b752a53b0537fd4c51f5fbd
Yucheng-An <yuc.an2019@gmail.com>
2024-06-05 13:22:17 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -9,16 +9,16 @@ const initialGameBoard = [
 
 
 export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIndex,colIndex){
-        setGameBoard((prevGameBoard) => {
-            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-            return updatedGameBoard;
-        })
-        onSelectSquare();
-    }
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    //
+    // function handleSelectSquare(rowIndex,colIndex){
+    //     setGameBoard((prevGameBoard) => {
+    //         const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //         updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedGameBoard;
+    //     })
+    //     onSelectSquare();
+    // }
 
     return <ol id="game-board">
         {gameBoard.map((row,rowIndex) =>
