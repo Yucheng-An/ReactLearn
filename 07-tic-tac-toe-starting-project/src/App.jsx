ea2dbb7842617a7891198d40622cfb01f4d10c0a
Yucheng-An <yuc.an2019@gmail.com>
2024-06-04 22:35:18 -0700

    Auto-commit JSX on save

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -5,18 +5,20 @@ function App() {
   return (
       <main>
         <div id = "game-container">
-          <ol id = "players">
-            <li>
-                <span className = "player-name">Player 1</span>
-                <span className = "player-symbol">X</span>
-            </li>
-              
-          </ol>
+            <ol id="players">
+                <li>
+                    <span className="player-name">Player 1</span>
+                    <span className="player-symbol">X</span>
+                </li>
+                <li>
+                    <span className="player-name">Player 1</span>
+                    <span className="player-symbol">X</span>
+                </li>
+            </ol>
         </div>
       </main>
   )
 }
-
 
 
 export default App
